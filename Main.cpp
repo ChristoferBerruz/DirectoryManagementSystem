776d45db00a1b6881397cdcd1957a3fb5645a28c
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-03-30 15:57:45 -0400

    Goodwin address fixed

--- Main.cpp
+++ Main.cpp
@@ -13,13 +13,13 @@ int main()
 	Contact* contacts[6];
 	contacts[0] = new BusinessAddressContact("University of Bridgeport", "University", "126 Park Ave, Bridgeport, CT 06604");
 	contacts[1] = new BusinessPhoneContact("Fairfield University", "University", "(800) 392-3582");
-	contacts[2] = new BusinessWebContact("Goodwin University", "University", "admissions@bridgeport.edu", "www.bridgeport.edu");
+	contacts[2] = new BusinessWebContact("Goodwin University", "University", "admissions@goodwin.edu", "www.goodwin.edu");
 	contacts[3] = new PersonAddressContact("Jhon Smith", "Male", "1491 Scelerisque Av. Fort Wayne, IN ,53759");
 	contacts[4] = new PersonEmailContact("Susana", "Female", "susana@gmail.com");
 	contacts[5] = new PersonPhoneContact("Vladimir", "Male", "1-176-320-8624");
 	for (int i = 0; i < 6; i++) {
 		contacts[i]->Display();
-		cout << "--------------------------" << endl;
+		cout << "--------------------------------" << endl;
 	}
 	return 0;
 }
