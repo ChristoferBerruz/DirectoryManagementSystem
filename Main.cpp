41b8271c23e5c6293289978f8306fc9d95f299cf
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-04-15 20:53:55 -0400

    Final test code

--- Main.cpp
+++ Main.cpp
@@ -3,11 +3,42 @@
 #include <fstream>
 #include "AreaCodesBook.h"
 using namespace std;
+
 int main()
 {
-	ifstream inputFile("input3.txt");
 	DirectoryManagementSystem dms;
-	dms.IngestData(inputFile);
-	dms.ShowAllContacts();
-	dms.Query();
+	cout << "Welcome to the Directory Management System" << endl;
+	cout << "Please enter the number of files: ";
+	int numFile;
+	cin >> numFile;
+
+	string* fileNames = new string[numFile];
+
+	cout << "Enter the file names one at a time: " << endl;
+	for (int i = 0; i < numFile; i++)
+	{
+		cin >> fileNames[i];
+		try {
+			ifstream inputFile(fileNames[i]);
+			dms.IngestData(inputFile);
+		}
+		catch (exception e)
+		{
+			cout << "Unable to read the file: " << e.what() << endl;
+		}
+	}
+
+	char response;
+	cout << "Querying the Directory Management System (DMS). Enter any key to continue. Enter n to stop: ";
+	cin >> response;
+
+	while (response != 'n')
+	{
+		dms.Query();
+		dms.DisplayResult();
+		cout << "Querying the Directory Management System (DMS). Press ENTER to continue. Enter n to stop: ";
+		cin >> response;
+	}
+
+	cout << "Exiting the Directory Management System." << endl;
 }
